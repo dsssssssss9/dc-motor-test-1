e1dde4801666ba074e4b00fc02a3c28a4493eb05
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-22 19:44:13 +0100

    digital write pin

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.digitalWritePin(DigitalPin.P0, 1)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.digitalWritePin(DigitalPin.P0, 0)
 })
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
 	
 })
